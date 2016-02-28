c8d882dbdf832f269ef228b848aec2d11d5be032
Samuel Brezani <samuel.brezani@globesy.sk>
2016-02-28 14:44:34 +0100

    Overload methods - add StringBuilder arg

--- zcommon/src/org/zkoss/json/JSONArray.java
+++ zcommon/src/org/zkoss/json/JSONArray.java
@@ -28,11 +28,14 @@ public class JSONArray extends LinkedList<Object> implements List<Object>, JSONA
 	 * @return JSON text, or "null" if list is null.
 	 */
 	public static String toJSONString(Collection collection){
+		return toJSONString(collection, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(Collection collection, StringBuilder sb){
 		if(collection == null)
-			return "null";
+			return sb.append("null");
 		
         boolean first = true;
-        StringBuilder sb = new StringBuilder();
 		Iterator iter=collection.iterator();
         
         sb.append('[');
@@ -50,141 +53,191 @@ public class JSONArray extends LinkedList<Object> implements List<Object>, JSONA
 			sb.append(JSONValue.toJSONString(value));
 		}
         sb.append(']');
-		return sb.toString();
+        return sb;
 	}
 	
 	/** Convert an object array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(Object[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(Object[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');
 	}
+	
 	/** Convert an integer array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(int[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(int[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');		
 	}
+	
 	/** Convert a long array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(long[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(long[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');		
 	}
+	
 	/** Convert a short array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(short[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(short[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');		
 	}
+	
 	/** Convert a float array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(float[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(float[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');		
 	}
+	
 	/** Convert a double array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(double[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(double[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
-	}
+		return sb.append(']');
+	}	
+	
 	/** Convert a byte array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(byte[] ary) {
-		if (ary == null)
-			return "null";
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
 
-		final StringBuilder sb = new StringBuilder().append('[');
+	public static StringBuilder toJSONString(byte[] ary, StringBuilder sb) {
+		if (ary == null)
+			return sb.append("null");
+
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');
 	}
+	
+	
 	/** Convert a boolean array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(boolean[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(boolean[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
-	}
+		return sb.append(']');
+	}	
+	
 	/** Convert a char array to JSON text.
 	 * <p>patched by tomyeh
 	 */
 	public static String toJSONString(char[] ary) {
+		return toJSONString(ary, new StringBuilder()).toString();
+	}
+	
+	public static StringBuilder toJSONString(char[] ary, StringBuilder sb) {
 		if (ary == null)
-			return "null";
+			return sb.append("null");
 
-		final StringBuilder sb = new StringBuilder().append('[');
+		sb.append('[');
 		for (int j = 0; j < ary.length; j++) {
 			if (j > 0) sb.append(',');
 			sb.append(JSONValue.toJSONString(ary[j]));
 		}
-		return sb.append(']').toString();
+		return sb.append(']');		
 	}
+	
 
 	/** Encodes this object to a JSON string.
 	 * It is the same as {@link #toString()}.
 	 */	
 	public String toJSONString(){
 		return toJSONString(this);
+	}
+	
+	public void toJSONString(StringBuilder sb) {
+		// TODO Auto-generated method stub
 	}
 	
 	/** Encodes this object to a JSON string.
